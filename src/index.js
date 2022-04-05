1890b3a0d2f299dae2919cb107b635c1b46a6b93
Leda <ledathemis@protonmail.com>
2022-04-05 17:26:51 +0300

    add option to have dropdown menu on hover

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './styles.css';
 
-const DropdownMenu = (id) => {
+const DropdownMenu = (id, mode) => {
+  const validModes = ['click', 'mouseover'];
+  if (!validModes.includes(mode)) {
+    throw `Valid modes are ${validModes}`;
+  }
+
   const DOMHandlers = (() => {
     const toggleDropdownMenu = (dropdownMenu) => {
       if (dropdownMenu.style.display) {
@@ -10,25 +15,37 @@ const DropdownMenu = (id) => {
       }
     };
 
-    const handleDropDownButtonClick = (e) => {
-      let dropdownMenu;
-      e.target.parentNode.childNodes.forEach((cn) => {
-        if (cn.classList && cn.classList.contains('dropdown-items')) {
-          dropdownMenu = cn;
-        }
-      });
+    const handleDropDownButtonClick = (e, id) => {
+      const dropdownMenu = document.querySelector(`#dropdown-items-${id}`);
       toggleDropdownMenu(dropdownMenu);
+    };
+
+    const handleDropDownDivMouseOver = (e, id) => {
+      const dropdownMenu = document.querySelector(`#dropdown-items-${id}`);
+      dropdownMenu.style.display = '';
+    };
+    const handleDropDownDivMouseLeave = (e, id) => {
+      const dropdownMenu = document.querySelector(`#dropdown-items-${id}`);
+      dropdownMenu.style.display = 'none';
     };
 
     return {
       handleDropDownButtonClick,
+      handleDropDownDivMouseOver,
+      handleDropDownDivMouseLeave,
     };
   })();
 
   const DOM = (() => {
-    const dropdownButton = document.querySelector(`#dropdown-button-${id}`);
-    dropdownButton.addEventListener('click', DOMHandlers.handleDropDownButtonClick);
+    if (mode === 'click') {
+      const dropdownButton = document.querySelector(`#dropdown-button-${id}`);
+      dropdownButton.addEventListener('click', (e) => DOMHandlers.handleDropDownButtonClick(e, id));
+    } else if (mode === 'mouseover') {
+      const dropdownDiv = document.querySelector(`#dropdown-menu-${id}`);
+      dropdownDiv.addEventListener('mouseover', (e) => DOMHandlers.handleDropDownDivMouseOver(e, id));
+      dropdownDiv.addEventListener('mouseleave', (e) => DOMHandlers.handleDropDownDivMouseLeave(e, id));
+    }
   })();
 };
 
-DropdownMenu(1);
+DropdownMenu(1, 'click');
