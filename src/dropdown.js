70c7fa398c3bf221621167de86b456fa6ebfd7f2
Leda <ledathemis@protonmail.com>
2022-04-05 17:40:34 +0300

    move dropdown module to dropdown.js

---
+++ src/dropdown.js
@@ -0,0 +1,56 @@
+import './dropdown.css';
+
+const DropdownMenu = (id, mode) => {
+  const validModes = ['click', 'mouseover'];
+  if (!validModes.includes(mode)) {
+    throw `Valid modes are ${validModes}`;
+  }
+
+  if (!document.querySelector(`#dropdown-menu-${id}`)) {
+    throw `A dropdown menu with id ${id} doesn't exist in HTML`;
+  }
+
+  const DOMHandlers = (() => {
+    const toggleDropdownMenu = (dropdownMenu) => {
+      if (dropdownMenu.style.display) {
+        dropdownMenu.style.display = '';
+      } else {
+        dropdownMenu.style.display = 'none';
+      }
+    };
+
+    const handleDropDownButtonClick = (e, id) => {
+      const dropdownMenu = document.querySelector(`#dropdown-items-${id}`);
+      toggleDropdownMenu(dropdownMenu);
+    };
+
+    const handleDropDownDivMouseOver = (e, id) => {
+      const dropdownMenu = document.querySelector(`#dropdown-items-${id}`);
+      dropdownMenu.style.display = '';
+    };
+    const handleDropDownDivMouseLeave = (e, id) => {
+      const dropdownMenu = document.querySelector(`#dropdown-items-${id}`);
+      dropdownMenu.style.display = 'none';
+    };
+
+    return {
+      handleDropDownButtonClick,
+      handleDropDownDivMouseOver,
+      handleDropDownDivMouseLeave,
+    };
+  })();
+
+  const DOM = (() => {
+    if (mode === 'click') {
+      const dropdownButton = document.querySelector(`#dropdown-button-${id}`);
+      dropdownButton.addEventListener('click', (e) => DOMHandlers.handleDropDownButtonClick(e, id));
+    } else if (mode === 'mouseover') {
+      const dropdownButton = document.querySelector(`#dropdown-button-${id}`);
+      const dropdownDiv = document.querySelector(`#dropdown-menu-${id}`);
+      dropdownButton.addEventListener('mouseover', (e) => DOMHandlers.handleDropDownDivMouseOver(e, id));
+      dropdownDiv.addEventListener('mouseleave', (e) => DOMHandlers.handleDropDownDivMouseLeave(e, id));
+    }
+  })();
+};
+
+export default DropdownMenu;
