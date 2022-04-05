96d32c9cd2c95c026c584c5153bcb07c6b21461b
Leda <ledathemis@protonmail.com>
2022-04-05 16:47:22 +0300

    add basic dropdown functionality

--- dist/main.js
+++ dist/main.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.css */ \"./src/styles.css\");\n\n\n\n//# sourceURL=webpack://odin-drop-down-menu/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.css */ \"./src/styles.css\");\n\n\nconst DropdownMenu = () => {\n  const DOMHandlers = (() => {\n    const toggleDropdownMenu = (dropdownMenu) => {\n      if (dropdownMenu.style.display) {\n        dropdownMenu.style.display = '';\n      } else {\n        dropdownMenu.style.display = 'none';\n      }\n    };\n\n    const handleDropDownButtonClick = (e) => {\n      let dropdownMenu;\n      e.target.parentNode.childNodes.forEach((cn) => {\n        if (cn.classList && cn.classList.contains('dropdown-items')) {\n          dropdownMenu = cn;\n        }\n      });\n      toggleDropdownMenu(dropdownMenu);\n    };\n\n    return {\n      handleDropDownButtonClick,\n    };\n  })();\n\n  const DOM = (() => {\n    const dropdownButton = document.querySelector('#dropdown-button-1');\n    dropdownButton.addEventListener('click', DOMHandlers.handleDropDownButtonClick);\n  })();\n};\n\nDropdownMenu();\n\n\n//# sourceURL=webpack://odin-drop-down-menu/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './styles.css';
+
+const DropdownMenu = () => {
+  const DOMHandlers = (() => {
+    const toggleDropdownMenu = (dropdownMenu) => {
+      if (dropdownMenu.style.display) {
+        dropdownMenu.style.display = '';
+      } else {
+        dropdownMenu.style.display = 'none';
+      }
+    };
+
+    const handleDropDownButtonClick = (e) => {
+      let dropdownMenu;
+      e.target.parentNode.childNodes.forEach((cn) => {
+        if (cn.classList && cn.classList.contains('dropdown-items')) {
+          dropdownMenu = cn;
+        }
+      });
+      toggleDropdownMenu(dropdownMenu);
+    };
+
+    return {
+      handleDropDownButtonClick,
+    };
+  })();
+
+  const DOM = (() => {
+    const dropdownButton = document.querySelector('#dropdown-button-1');
+    dropdownButton.addEventListener('click', DOMHandlers.handleDropDownButtonClick);
+  })();
+};
+
+DropdownMenu();
